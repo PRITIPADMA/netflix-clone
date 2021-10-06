887296bd2580ac323f54edd9982442e5f0b13bfa
PRITIPADMA <pritipadmamishra38@gmail.com>
2021-10-06 08:35:12 +0000

    third commmit

--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from "./Card"
+import "./index.css"
 
 ReactDOM.render(
   <>
+  <h1 className="heading_style">HEIGALA HEIGALA RE</h1>
    <Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" title="A Netflix Original"
    sname='DARK'
    link="https://www.netflix.com/in/title/80990668?source=35"
