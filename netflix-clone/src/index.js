0317b61549c4876ec493996c35974c5a3d223626
PRITIPADMA <pritipadmamishra38@gmail.com>
2021-10-06 08:06:59 +0000

    second commit

--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -1,42 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Card from "./Card"
 
 ReactDOM.render(
   <>
-    <div className="cards">
-      <div className="card">
-        <img 
-          src="https://wallpapercave.com/wp/wp4056410.jpg" 
-          alt="myPic" 
-          className="card__img" 
-        />
-        <div className="card__info">
-          <span className="card__category">A Netflix Original</span>
-            <h3 className="card__title">DARK</h3>
-              <a href="https://www.netflix.com/in/title/80990668?source=35" target="_blank">
-                <button>Watch Now</button>
-              </a>
-        </div>
-      </div>
-    </div>
-
-    <div className="cards">
-      <div className="card">
-        <img 
-          src="https://wallpapercave.com/wp/wp4056410.jpg" 
-          alt="myPic" 
-          className="card__img" 
-        />
-        <div className="card__info">
-          <span className="card__category">A Netflix Original</span>
-            <h3 className="card__title">DARK</h3>
-              <a href="https://www.netflix.com/in/title/80990668?source=35" target="_blank">
-                <button>Watch Now</button>
-              </a>
-        </div>
-      </div>
-    </div>
+   <Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" title="A Netflix Original"
+   sname='DARK'
+   link="https://www.netflix.com/in/title/80990668?source=35"
+   
+   />
+   <Card
+   imgsrc="https://wallpapercave.com/wp/wp8988425.jpg" 
+   title="A Netflix Original"
+   sname='Stranger Things'
+   link="https://www.netflix.com/in/title/80990668?source=35"
+   />
+   <Card
+   imgsrc="https://wallpapercave.com/uwp/uwp1470273.jpeg" title="A Netflix Original"
+   sname='Extra Curricular'
+   link="https://www.netflix.com/in/title/80990668?source=35"
+   />
   </>,
-  document.getElementById('root')
-);
+document.getElementById('root'));
